93a681e920d9af6bf075355306a14148baabc918
mireyachaparro <mireyacn99@gmail.com>
2022-12-06 19:06:34 +0100

    Add reducer test

---
+++ src/features/reducer/reducer.test.ts
@@ -0,0 +1,106 @@
+import { albumReducer } from './reducer';
+import { actionTypes } from './action.types';
+import { Album } from '../models/album.model';
+
+describe('given albumReducer', () => {
+    const albumMock: Album = {
+        id: '1',
+        name: '',
+        artist: '',
+        image: '',
+        year: 1,
+        gender: '',
+        format: '',
+        price: 1,
+        sold: false,
+    };
+
+    let state: Array<Album>;
+    let action: { type: string; payload: unknown };
+
+    describe('when action is load', () => {
+        test('then it should return state with action payload', () => {
+            action = {
+                type: actionTypes.load,
+                payload: [albumMock],
+            };
+            state = [];
+            const result = albumReducer(state, action);
+            expect(result).toEqual(action.payload);
+        });
+    });
+
+    describe('when action is add', () => {
+        test('then it should return state including the action payload', () => {
+            action = {
+                type: actionTypes.add,
+                payload: albumMock,
+            };
+            state = [];
+            const result = albumReducer(state, action);
+            expect(result).toContainEqual(action.payload);
+        });
+    });
+
+    describe('when action is update with a valid id', () => {
+        test('then it should return state including the action payload', () => {
+            action = {
+                type: actionTypes.update,
+                payload: { ...albumMock, title: 'updated album' },
+            };
+            state = [albumMock];
+            const result = albumReducer(state, action);
+            expect(result).toContainEqual(action.payload);
+        });
+    });
+
+    describe('when action is update with an invalid id', () => {
+        test('then it should return state with original state', () => {
+            action = {
+                type: actionTypes.update,
+                payload: { ...albumMock, title: 'updated album', id: '2' },
+            };
+            state = [albumMock];
+            const result = albumReducer(state, action);
+            expect(result).toEqual(state);
+        });
+    });
+
+    describe('when action is delete with a valid id', () => {
+        test('then it should return state not including the action payload', () => {
+            action = {
+                type: actionTypes.delete,
+                payload: albumMock.id,
+            };
+            state = [];
+
+            const result = albumReducer(state, action);
+            expect(result).toEqual([]);
+        });
+    });
+
+    describe('when action is delete with an invalid id', () => {
+        test('then it should return state with original state', () => {
+            action = {
+                type: actionTypes.delete,
+                payload: { ...albumMock, id: '2' },
+            };
+            state = [albumMock];
+
+            const result = albumReducer(state, action);
+            expect(result).toEqual(state);
+        });
+    });
+
+    describe('when action is any other', () => {
+        action = {
+            type: '',
+            payload: null,
+        };
+        state = [albumMock];
+        test('', () => {
+            const result = albumReducer(state, action);
+            expect(result).toEqual(state);
+        });
+    });
+});
