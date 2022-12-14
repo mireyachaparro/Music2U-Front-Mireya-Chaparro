6c3f3159b0140e6f64f90a417570e7b76a4639d8
mireyachaparro <mireyacn99@gmail.com>
2022-12-14 21:27:06 +0100

    Fix duplicate code sonar

--- src/infrastructure/components/cd/page/cd.page.test.tsx
+++ src/infrastructure/components/cd/page/cd.page.test.tsx
@@ -1,39 +1,11 @@
-import { configureStore } from '@reduxjs/toolkit';
 import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { MemoryRouter as Router } from 'react-router-dom';
-import { albumReducer } from '../../../../features/album/reducer/album.reducer';
-import { userReducer } from '../../../../features/user/reducer/user.reducer';
-import { rootState, rootStore } from '../../../store/store';
+import { mockStore } from '../../../../mock/mocks';
 import CdPage from './cd.page';
 
 describe('Given Cd page', () => {
     describe('When we render the component', () => {
-        const preloadedState: rootState = {
-            albums: [],
-            users: {
-                token: '',
-                isLogged: true,
-                isLogging: false,
-                user: {
-                    id: '',
-                    name: '',
-                    last_name: '',
-                    email: '',
-                    password: '',
-                    phone: '',
-                    birthday: '',
-                    favorites: [],
-                    possessions: [],
-                },
-            },
-        };
-
-        const mockStore: rootStore = configureStore({
-            reducer: { albums: albumReducer, users: userReducer },
-            preloadedState,
-        });
-
         beforeEach(() => {
             render(
                 <Provider store={mockStore}>
--- src/infrastructure/components/details/details.test.tsx
+++ src/infrastructure/components/details/details.test.tsx
@@ -1,60 +1,14 @@
-import { configureStore } from '@reduxjs/toolkit';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { Provider } from 'react-redux';
 import { MemoryRouter as Router } from 'react-router-dom';
-import { AlbumModel } from '../../../features/album/model/album.model';
-import { albumReducer } from '../../../features/album/reducer/album.reducer';
 import { useUsers } from '../../../features/user/hooks/use.users';
-import { userReducer } from '../../../features/user/reducer/user.reducer';
-import { rootState, rootStore } from '../../store/store';
+import { mockStore } from '../../../mock/mocks';
 import { AlbumDetails } from './details';
-import { mockPass } from '../../../mock/mocks';
 
 jest.mock('../../../features/user/hooks/use.users');
 
 describe('Given AlbumDetails page', () => {
-    const preloadedState: rootState = {
-        albums: [
-            {
-                ...new AlbumModel('', '', '', 1, '', '', 1, false),
-                id: '1',
-                owner: {
-                    id: '',
-                    name: '',
-                    last_name: '',
-                    email: '',
-                    password: '',
-                    phone: '',
-                    birthday: '',
-                    favorites: [],
-                    possessions: [],
-                },
-            },
-        ],
-        users: {
-            token: '',
-            isLogged: true,
-            isLogging: false,
-            user: {
-                id: '1',
-                name: 'a',
-                last_name: 'a',
-                email: 'a@a',
-                password: mockPass,
-                phone: '1',
-                birthday: '1',
-                favorites: [],
-                possessions: [],
-            },
-        },
-    };
-
-    const mockStore: rootStore = configureStore({
-        reducer: { albums: albumReducer, users: userReducer },
-        preloadedState,
-    });
-
     describe('When we render the component', () => {
         beforeEach(() => {
             (useUsers as jest.Mock).mockReturnValue({
